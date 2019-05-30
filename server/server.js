3fe68012b0c0bf2bd905f3f2cecbf4e45bdad291
tombuyse <tom.buyse5@gmail.com>
2019-05-30 11:42:08 +0200

    updates server.js to use imports

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import appModulePath from 'app-module-path';
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
 
 appModulePath.addPath(`${__dirname}`);
 
-console.log('start server');
-const Api = require('express')();
-const HTTP = require('http').Server(Api);
-const Cors = require('cors');
+const Api = express();
+const HTTP = http.Server(Api);
 
-Api.use(Cors());
+Api.use(cors());
 
 Api.get('/test', (req, res) => res.status(200).send('success!'));
 
